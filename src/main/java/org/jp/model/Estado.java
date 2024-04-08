97ca995697a2681d59dc6003bc178aa71d8d6b82
Luahr Rocha <73291538+luahr13@users.noreply.github.com>
2024-04-08 20:07:29 -0300

    Classe Estado

---
+++ src/main/java/org/jp/model/Estado.java
@@ -0,0 +1,58 @@
+package org.jp.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+public class Estado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "sigla", nullable = false, length = 2)
+    private String sigla;
+
+    @OneToMany(mappedBy = "estado", cascade = CascadeType.ALL)
+    private List<Cidade> cidades = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public List<Cidade> getCidades() {
+        return cidades;
+    }
+
+    public void setCidades(List<Cidade> cidades) {
+        this.cidades = cidades;
+    }
+}
